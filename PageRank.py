111f4ce0fd483dac3369e49d4875e27d2a8cb9e8
jakubmraz <jakub.mraz111@gmail.com>
2022-11-08 15:00:36 +0100

    Read shit

--- PageRank.py
+++ PageRank.py
@@ -1,1 +1,16 @@
-print("Yello world")
+import numpy as np
+import networkx as nx
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+
+file = open("PageRankExampleData\medium.txt", "rb")
+G: nx.Graph = nx.read_adjlist(file, create_using=nx.DiGraph())
+file.close()
+
+def PageSurfer():
+    pass
+
+def CalculateGraphSize(graph):
+    return [len(G), len(G)]
+
+print(CalculateGraphSize(G))
